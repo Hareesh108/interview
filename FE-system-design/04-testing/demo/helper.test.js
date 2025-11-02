dca8f6ad63f621363abbf2dea00c3e5db83be4b3
Hareesh108 <hareeshbhittam@gmail.com>
2025-11-02 19:50:48 +0530

    Testing demo with jest

--- FE-system-design/04-testing/demo/helper.test.js
+++ FE-system-design/04-testing/demo/helper.test.js
@@ -2,14 +2,19 @@ test("sortMe function", () => {
   const { sortMe } = require("./helper");
   const sorted = sortMe();
 
-//   expect(sorted).toEqual([
-//     { name: "Keshav", age: 16 },
-//     { name: "Harsh", age: 20 },
-//     { name: "John", age: 25 },
-//     { name: "Rahul", age: 28 },
-//     { name: "Jane", age: 30 },
-//   ]);
+  expect(sorted).toEqual([
+    { name: "Keshav", age: 16 },
+    { name: "Harsh", age: 20 },
+    { name: "John", age: 25 },
+    { name: "Rahul", age: 28 },
+    { name: "Jane", age: 30 },
+  ]);
 
-//   expect(sorted[0]).not.toEqual(30);
-  expect(sorted[1]).toEqual(20);
+});
+
+test("sortMe function", () => {
+  const { sortMe } = require("./helper");
+  const sorted = sortMe();
+
+  expect(sorted[0]).toEqual(30);
 });

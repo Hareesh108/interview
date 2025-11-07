c49b4d01b413557c8e2305065d575311972e42f2
Hareesh108 <hareeshbhittam@gmail.com>
2025-11-07 23:30:21 +0530

    Worked on e2e testing

--- FE-system-design/04-testing/demo/e2e.js
+++ FE-system-design/04-testing/demo/e2e.js
@@ -14,8 +14,8 @@ const puppeteer = require("puppeteer");
 
   await page.setViewport({ width: 1920, height: 1080 });
 
-  const selector =
-    "body > div.relative.min-h-screen > div.fixed.bottom-8.left-1/2.transform.-translate-x-1/2.z-50.undefined > div > div.relative.bg-white/10.dark:bg-black/10.backdrop-blur-xl.border.border-white/20.dark:border-white/10.rounded-2xl.transition-all.duration-500.overflow-hidden.shadow-2xl > div.relative.px-6.py-3 > div > div.flex.items-center.justify-center.w-10.h-10.bg-white/20.dark:bg-white/10.backdrop-blur-md.border.border-white/30.dark:border-white/20.hover:bg-white/30.dark:hover:bg-white/20.rounded-xl.transition-all.duration-300.shadow-lg";
+  // const selector =
+    // "body > div.relative.min-h-screen > div.fixed.bottom-8.left-1/2.transform.-translate-x-1/2.z-50.undefined > div > div.relative.bg-white/10.dark:bg-black/10.backdrop-blur-xl.border.border-white/20.dark:border-white/10.rounded-2xl.transition-all.duration-500.overflow-hidden.shadow-2xl > div.relative.px-6.py-3 > div > div.flex.items-center.justify-center.w-10.h-10.bg-white/20.dark:bg-white/10.backdrop-blur-md.border.border-white/30.dark:border-white/20.hover:bg-white/30.dark:hover:bg-white/20.rounded-xl.transition-all.duration-300.shadow-lg";
 
   await page.waitForSelector(selector);
 

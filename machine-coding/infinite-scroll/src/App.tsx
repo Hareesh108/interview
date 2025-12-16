8337c51a51cbd849168eb8352322bfe5590019f9
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-16 15:20:56 +0530

    Added UI

---
+++ machine-coding/infinite-scroll/src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [memes, setMemes] = useState([]);
+
+  const fetchMemes = async () => {
+    try {
+      const res = await fetch("https://meme-api.com/gimme/15");
+      const data = await res.json();
+      setMemes((prevMemes) => [...prevMemes, ...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;

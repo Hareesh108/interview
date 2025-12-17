429b38d1b30148b6319b4a7e58d64f686c015353
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-17 20:05:45 +0530

    Study: array contructor and four static methods

---
+++ js/array/array-instance-methods.ts
@@ -0,0 +1,4 @@
+const me = ["Hareesh", "Parasu"];
+const num = [1, 2, 3, 4];
+
+console.log(0 in me);
---
+++ js/array/array-static-methods.ts
@@ -0,0 +1,31 @@
+// const test = new Array()
+const test = Array();
+
+console.log(test);
+
+// console.log(Array.from("foo"));
+
+const set = new Set([1, 2, 3]);
+Array.from(set);
+
+const str = "a1b2c3";
+
+const digits = Array.from(str.matchAll(/\d/g), (match) => Number(match[0]));
+
+const multiplier = {
+  factor: 10,
+};
+
+const result = Array.from(
+  [1, 2, 3],
+  function (num) {
+    return num * this.factor;
+  },
+  multiplier
+);
+
+console.log(result);
+
+
+const test2 = []
+console.log(0 in test2);

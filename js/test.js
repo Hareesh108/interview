c2e2c25c8dcc762af08479c2e2b104011e311a30
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-21 22:39:31 +0530

    Added nested comments using react

--- js/test.js
+++ js/test.js
@@ -1,58 +1,15 @@
-setTimeout(() => {
-  console.log("Alex"); // 10
-}, 0); // (1 macro)
+// for (var i = 0; i < 4; i++) {
+//   ((g) => {
+//     setTimeout(() => {
+//       console.log(g);
+//     }, 1000);
+//   })(i);
+// }
 
-const id = setInterval(() => {
-  console.log("Blake"); // 11 , 12
-  clearInterval();
-}, 1000); // (2 macro)
+// console.log(i);
 
-console.log("Chris"); // 1
 
-(function () {
-  console.log("Dana"); // 2
-})(); 
+const callme = h1 => h2 => h3 => h1 + h2 + h3
 
-queueMicrotask(() => {
-  console.log("Evan"); // 7
-}); // (1 micro) -> done after 7
-
-function normalFn() {
-  console.log("Finn"); // 3
-}
-normalFn(); 
-
-async function asyncFn() {
-  console.log("Gray"); // 4
-  await null; // (2 micro) 
-  console.log("Harper"); // 8
-}
-asyncFn();
-
-new Promise((resolve) => {
-  console.log("Indy"); // 5
-  resolve();
-}).then(() => { // (3 micro)
-  console.log("Jules"); // 9
-});
-
-setTimeout(() => {
-  clearInterval(id);
-  console.log("Kai"); // (3 macro) ->  13
-}, 2100); 
-
-console.log("Logan"); // 6
-
-// Chris
-// Dana
-// Finn
-// Gray
-// Indy
-// Logan
-// Evan
-// Harper
-// Jules
-// Alex
-// Blake
-// Blake
-// Kai
+const new1 = callme(2)(3)(1)
+console.log(new1);

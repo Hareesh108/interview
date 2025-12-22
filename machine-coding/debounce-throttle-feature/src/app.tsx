985d3d0d6076ace8aefb4708b1065de178432ae0
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-22 17:28:31 +0530

    Added debounce logic

---
+++ machine-coding/debounce-throttle-feature/src/app.tsx
@@ -0,0 +1,12 @@
+import BasicDebounce from "./components/debounce/basic";
+
+function App() {
+  return (
+    <>
+      <BasicDebounce />
+      {/* <BasicDebounce /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ machine-coding/debounce-throttle-feature/src/components/debounce/optimize.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+function OptimizeDebounce() {
+  const [params, setParams] = useState("");
+  const [debounceText, setDebounceText] = useState("");
+
+  console.log("params:", params);
+  console.log("debounceText:", debounceText);
+
+  const fetchMe = async (p?: string) => {
+    console.log("params:", p);
+
+    try {
+      const res = await Promise.resolve("Success");
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchMe(debounceText.trim());
+  }, [debounceText]);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setDebounceText(params);
+    }, 4000);
+
+    console.log("setTimeoutID:", id);
+
+    return () => clearTimeout(id);
+  }, [params]);
+
+  return (
+    <div className="w-xl m-auto mt-40">
+      <div className="flex flex-col gap-2 ">
+        <label
+          htmlFor="search"
+          className="text-center font-bold text-lg hover:border-pink-400"
+        >
+          Testing Optimize Debounce
+        </label>
+        <input
+          name="search"
+          className="border p-1"
+          onChange={(e) => setParams(e.target.value)}
+        ></input>
+      </div>
+    </div>
+  );
+}
+
+export default OptimizeDebounce;

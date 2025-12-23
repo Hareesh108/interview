9b9c2967833603d7768dcd70cb9a9e6ddac33347
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-23 21:51:45 +0530

    Added Promose.all()

--- js/promise/static-methods/all.ts
+++ js/promise/static-methods/all.ts
@@ -1,4 +1,42 @@
-const colors = fetch('https://mdn.github.io/js-examples/module-examples/top-level-await/data/colors.json')
-	.then(response => response.json());
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Resolved1");
+  }, 2000);
+});
 
-export default await colors;
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Reject");
+  }, 2000);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Resolved2");
+  }, 3000);
+});
+
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Resolved3");
+  }, 3000);
+});
+
+Promise.all([promise1, promise2, promise3, promise4])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
+
+const fetchAll = async () => {
+  try {
+    const res = await Promise.all([promise1, promise3, promise4]);
+    console.log(res);
+  } catch (e) {
+    console.log("Error:", e);
+  }
+};
+
+fetchAll();

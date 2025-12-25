9094f1e4270d080d51866fd431caabfcac79c931
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-25 13:57:49 +0530

    flow added

--- machine-coding/infinite-scroll/src/App.tsx
+++ machine-coding/infinite-scroll/src/App.tsx
@@ -1,12 +1,27 @@
+import { Profiler } from "react";
 import Basic from "./components/basic";
 import IntersectionObserver from "./components/intersection-observer";
 
+function onRender({
+  id,
+  phase,
+  actualDuration,
+  baseDuration,
+  startTime,
+  commitTime,
+}: any) {
+  // Aggregate or log render timings...
+  console.log("Hii");
+  
+  console.log(id, phase, actualDuration, baseDuration, startTime, commitTime);
+}
+
 function App() {
   return (
-    <>
+    <Profiler id="App" onRender={onRender}>
       {/* <Basic /> */}
       <IntersectionObserver />
-    </>
+    </Profiler>
   );
 }
 

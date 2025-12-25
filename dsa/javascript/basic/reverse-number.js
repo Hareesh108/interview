d9262d5d3f70196e15e906dac5fd5c37efb4082e
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-26 01:34:38 +0530

    Added reverse number code

---
+++ dsa/javascript/basic/reverse-number.js
@@ -0,0 +1,13 @@
+const reverseNumber = (n) => {
+  let rev = 0;
+
+  while (n > 0) {
+    const digit = n % 10;
+    rev = rev * 10 + digit;
+    n = Math.floor(n / 10);
+  }
+
+  return rev;
+};
+
+console.log(reverseNumber(1234));

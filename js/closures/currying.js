decf7b151824272aeb80f9cf4f3a07707d39aa86
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-29 00:31:32 +0530

    CUrrying example

--- js/closures/currying.js
+++ js/closures/currying.js
@@ -1,6 +1,7 @@
 // infinite currying 
 // Use case Accepts unlimited inputs, No arrays needed
 // Used in ORMs, Query builders, Configuration APIs
+
 function add(a) {
   return function (b) {
     if (b === undefined) return a;
@@ -8,4 +9,6 @@ function add(a) {
   };
 }
 
-add(1)(2)(3)(4)();
+console.log(add(1)(2)(3)(4)());
+
+console.log(add(10)(20)(30)(40)(50)(60)());

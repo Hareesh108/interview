0f547a0169cbc55522025224672fe7dd9cc21768
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-29 12:49:36 +0530

    Study: debounce and throttle

--- machine-coding/debounce-throttle-feature/src/app.tsx
+++ machine-coding/debounce-throttle-feature/src/app.tsx
@@ -1,13 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import BasicDebounce from "./components/debounce/basic";
 import OptimizeDebounce from "./components/debounce/optimize";
 import BasicThrottle from "./components/throttle/basic";
 
+const useDebounce = (text: string, time: number) => {
+  const [myText, setMyText] = useState("");
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setMyText(text);
+    }, time);
+
+    return () => clearTimeout(id);
+  }, [text, time]);
+
+  return myText;
+};
+
+const useThrottle = (text: number, time: number) => {
+  const [myThrottleText, setMyThrottleText] = useState(0);
+
+  const lastRun = useRef(0);
+
+  useEffect(() => {
+    const now = Date.now();
+
+    if (now - lastRun.current >= time) {
+      setMyThrottleText(text);
+      lastRun.current = now;
+    }
+  }, [text, time]);
+
+  return myThrottleText;
+};
+
 function App() {
+  // const [myText, setMyText] = useState(0);
+
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setMyText((prev) => prev + 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(id);
+  // }, []);
+
+  // const throttled = useThrottle(myText, 4000);
+
+  // console.log("original:", myText, "throttled:", throttled);
+
   return (
     <>
-      {/* <BasicDebounce />
-      <OptimizeDebounce /> */}
-      <BasicThrottle/>
+      <BasicDebounce />
+      {/* <OptimizeDebounce /> */}
+      {/* <BasicThrottle/> */}
     </>
   );
 }

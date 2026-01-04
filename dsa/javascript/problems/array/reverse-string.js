5bf9f6d36e7f6d477de68f950572eac106c1ab0e
Hareesh108 <hareeshbhittam@gmail.com>
2026-01-04 15:53:53 +0530

    Remove duplicate from array

--- dsa/javascript/problems/array/reverse-string.js
+++ dsa/javascript/problems/array/reverse-string.js
@@ -1,21 +1,24 @@
-const s = ["h","e","l","l","o"]
+const s = ["h", "e", "l", "l", "o"];
 
 // IMP
 const reverseString = function (s) {
+  const size = s.length;
 
-    const size = s.length
+  for (let i = 0; i < size / 2; i++) {
+    let temp = s[size - 1 - i];
+    s[size - 1 - i] = s[i];
+    s[i] = temp;
+  }
 
-    for (let i = 0; i < size / 2; i++) {
-        let temp = s[size - 1 - i]
-        s[size - 1 - i] = s[i]
-        s[i] = temp
-    }
+//   WITHOUT THIRD VARIABLE
+//   for (let i = 0; i < size / 2; i++) {
+//     [s[i], s[size - 1 - i]] = [s[size - 1 - i], s[i]];
+//   }
 
-    return s
+  return s;
 };
 
 console.log(reverseString(s));
 
-const s1 = ["h","e","l","l","o"]
+const s1 = ["h", "e", "l", "l", "o"];
 console.log(s1.toReversed());
-

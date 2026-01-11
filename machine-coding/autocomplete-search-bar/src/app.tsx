6c2649cb875dc1997d8f7c10408b2444ee1dc4c0
Hareesh108 <hareeshbhittam@gmail.com>
2026-01-11 23:28:56 +0530

    Added search api from dummy

--- machine-coding/autocomplete-search-bar/src/app.tsx
+++ machine-coding/autocomplete-search-bar/src/app.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [search, setSearch] = useState("");
-  console.log(search);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState();
+  const [view, setView] = useState(false);
+  // console.log(search);
 
   useEffect(() => {
     const fetchApi = async () => {
       try {
         const res = await fetch(
-          "https://dummyjson.com/recipes/search?q=Margherita"
+          "https://dummyjson.com/recipes/search?q=" + text
         );
         const json = await res.json();
         setSearch(json);
@@ -18,12 +18,44 @@ function App() {
         console.log(e);
       }
     };
-    fetchApi();
-  }, []);
+
+    const timer = setTimeout(() => {
+      fetchApi();
+    }, 300);
+    console.log("outer", timer);
+
+    return () => {
+      console.log("inner", timer);
+      clearTimeout(timer);
+    };
+  }, [text]);
 
   return (
-    <div>
-      <div></div>
+    <div className="max-w-xl mx-auto mt-20">
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="border-2 w-full"
+          onFocus={() => setView(true)}
+          onBlur={() => setView(false)}
+        />
+      </div>
+
+      <div className="border max-h-72 overflow-y-scroll">
+        {search?.recipes?.map((item) => (
+          <button
+            key={item.id}
+            className="hover:bg-red-300 block w-full text-start px-1 py-1 cursor-pointer"
+            onClick={() => {
+              setText(item.name);
+            }}
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }

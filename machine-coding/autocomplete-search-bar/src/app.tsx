dbd98908ae91cb3d2a24be35d303a09470b54ec6
Hareesh108 <hareeshbhittam@gmail.com>
2026-08-20 16:56:34 +0530

    System Design

--- machine-coding/autocomplete-search-bar/src/app.tsx
+++ machine-coding/autocomplete-search-bar/src/app.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Basic from "./basic";
-import Better from "./better";
+import React from 'react';
+import Basic from './basic';
+import Better from './better';
 
 const App = () => {
   return (
     <div>
-      {/* <Basic /> */}
-      <Better />
+      <Basic />
+      {/* <Better /> */}
     </div>
   );
 };
